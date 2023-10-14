d3746ad87fca567c02338d3a0e0d83109ddfdaa9
ShawonSUSTSWE <shawon2046@gmail.com>
2023-10-15 01:45:23 +0600

    Requesting Appointment Done

--- my-app/src/lib/service/appointment.js
+++ my-app/src/lib/service/appointment.js
@@ -6,10 +6,11 @@ const requestAppointment = async (req, res) => {
   const session = await getServerSession(req, res, createOptions(req));
   console.log(session);
   const appointmentData = req.body;
-  //   const { __v, ...createdAppointment } = await appointment
-  //     .create(appointmentData)
-  //     .then((obj) => obj.toObject());
-  return res.status(200).json(req.body);
+  appointmentData.consultee = session.user._id;
+  const { __v, ...createdAppointment } = await appointment
+    .create(appointmentData)
+    .then((obj) => obj.toObject());
+  return res.status(201).json(createdAppointment);
 };
 
 export { requestAppointment };
