aab442c231f2a3388fb271224497beeecdd9bdd6
ShawonSUSTSWE <shawon2046@gmail.com>
2023-10-14 12:27:18 +0600

    User model created

--- my-app/src/models/user.js
+++ my-app/src/models/user.js
@@ -1,3 +1,29 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: Date,
+  gender: String,
+  bloodGroup: {
+    type: String,
+    enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+  },
+  mobileNumber: String,
+  address: String,
+  city: String,
+  country: String,
+});
+
+export default mongoose.models?.User ?? mongoose.model("User", userSchema);
